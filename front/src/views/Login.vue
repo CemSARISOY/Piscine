f65a2f25e850d6b71df5a2178084bc5c7f65369c
Cem SARISOY <cem.sarisoy@etu.umontpellier.fr>
2020-12-03 19:33:39 +0100

    feat: login page

---
+++ front/src/views/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class=login>
+    <h1>Connexion</h1>
+    <login-form/>
+  </div>
+</template>
+<script>
+import loginForm from "../components/loginForm"
+export default {
+  components:{
+    loginForm
+  }
+}
+</script>
