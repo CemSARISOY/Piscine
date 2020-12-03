f4fac75ff72feb73ab53d9c971af501dfff33c0d
Cem SARISOY <cem.sarisoy@etu.umontpellier.fr>
2020-12-03 19:08:15 +0100

    build: bootstrap

--- front/src/main.js
+++ front/src/main.js
@@ -1,4 +1,7 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -6,7 +9,7 @@ import store from './store'
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
