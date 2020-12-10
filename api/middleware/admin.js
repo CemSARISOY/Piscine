232ae17daa1cfebb6d4581e8026b0d9eba8a659a
Cem SARISOY <cem.sarisoy@etu.umontpellier.fr>
2020-12-10 19:59:13 +0100

    feat: session based auth

--- api/middleware/admin.js
+++ api/middleware/admin.js
@@ -2,15 +2,9 @@ const jwt = require('jsonwebtoken');
 
 // Only If Admin Next()
 module.exports = (req, res, next) => {
-    let cookie = req.cookies.sessionCookie;
-    try{
-        const decoded = jwt.verify(cookie, process.env.RANDOMSECRETTOKEN);
-        if ( decoded.isAdmin ) {
-            next();
-        } else {
-            throw 'Accès refusé';
-        }
-    } catch {
-        res.status(401).json({err: 'Requête invalide'});
+    if(req.session.isAdmin){
+        next();
+    }else{
+        res.status(403).json({err: "Non autorisé"});
     }
 }
--- api/routes/etudiants.js
+++ api/routes/etudiants.js
@@ -7,13 +7,15 @@ const admin = require('../middleware/admin');
 
 const etudiantCtrl = require("../controllers/etudiants");
 
+router.post("/", etudiantCtrl.createEtudiant);
+router.post("/login", etudiantCtrl.login);
+router.post("/session", etudiantCtrl.session);
+
 router.get("/", auth, etudiantCtrl.getAllEtudiant);
 router.get("/:id", auth, etudiantCtrl.getOneEtudiant);
-router.post("/", etudiantCtrl.createEtudiant);
 router.delete("/:id", auth, etudiantCtrl.deleteEtudiant);
 router.put("/:id", auth, etudiantCtrl.updateEtudiant);
 
-router.post("/login", etudiantCtrl.login);
 router.get("/promo/:promo", admin, etudiantCtrl.getEtudiantsInPromo);
 
 
