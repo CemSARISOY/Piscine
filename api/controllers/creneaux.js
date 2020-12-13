b4f4498d9763c1dcf5bcd66e297888efa9f48165
Gachignard Arthur <arthur.gachignard@etu.umontpellier.fr>
2020-12-13 15:56:33 +0100

    first commit

---
+++ api/controllers/creneaux.js
@@ -0,0 +1,97 @@
+const Creneaux = require("../model/creneauxModel");
+
+exports.getAllCreneaux = async (req, res) => {
+    try{
+        const creneaux = await Creneaux.selectAll()
+        if(creneaux.rowCount > 0){
+            res.status(200).json(creneauxs.rows);
+        }else{
+            res.status(404).json({message : "Il n'existe aucun créneau"});
+        }
+    }catch(err){
+        res.status(500).json({message : err.message});
+    }
+};
+
+exports.getByEvent = async (req, res) => {
+    try{
+        const creneaux = await Creneaux.selectByEvent(req.params.idEvenement)
+        if(creneaux.rowCount == 1){
+            res.status(200).json(creneaux.rows[0])
+        }else{
+            res.status(404).json({message : "Il n'existe aucun créneau sur cet évènement"});
+        }
+    }catch(err){
+        res.status(500).json({message : err.message});
+    }
+};
+
+exports.createCreneaux = async (req, res) => {
+    try{
+        let data = req.body;
+        const result = await Creneaux.create(data);
+        if(result.rowCount == 1){
+            res.status(201).json(result.rows[0])
+        }else{
+            res.status(404).json({message : "Erreur de création de créneau"});
+        }
+        
+    }catch(err){
+        res.status(500).json({message : err.message});
+    }
+};
+
+exports.deleteCreneaux = async (req, res) => {
+    try{
+        const result = await Creneaux.delete(req.params.idCreneau);
+        if(result.rowCount == 1){
+            res.status(200).json(result.rows[0]);
+        }else{
+            res.status(400).json({message : "Erreur de suppression de créneau"});
+        }
+    }catch(err){
+        res.status(500).json({message : err.message});
+    }
+};
+
+exports.updateCreneaux = async (req, res) => {
+    try{
+        let data = req.body;
+        const result = await Creneaux.update(data, req.params.idCreneau);
+        if(result.rowCount == 1){
+            res.status(200).json(result.rows[0])
+        }else{
+            res.status(400).json({message : "Erreur de modification du créneau"});
+        }
+    }catch(err){
+        res.status(500).json({message : err.message});
+    }
+};
+
+exports.reserverCreneaux = async (req, res) => {
+    try{
+        let data = req.body;
+        const result = await Creneaux.reserver(req.params.idCreneau);
+        if(result.rowCount == 1){
+            res.status(200).json(result.rows[0])
+        }else{
+            res.status(400).json({message : "Erreur de réservation du créneau"});
+        }
+    }catch(err){
+        res.status(500).json({message : err.message});
+    }
+};
+
+exports.annulerReservationCreneaux = async (req, res) => {
+    try{
+        let data = req.body;
+        const result = await Creneaux.annulerRes(req.params.idCreneau);
+        if(result.rowCount == 1){
+            res.status(200).json(result.rows[0])
+        }else{
+            res.status(400).json({message : "Erreur d'annulation du créneau"});
+        }
+    }catch(err){
+        res.status(500).json({message : err.message});
+    }
+};
--- api/routes/evenements.js
+++ api/routes/evenements.js
@@ -3,7 +3,23 @@
 const express = require("express");
 const router = express.Router();
 const pool = require("../db");
+const auth = require('../middleware/auth');
+const admin = require('../middleware/admin');
 
+const creneauxCtrl = require("../controllers/creneaux");
 
+//Récupérer les créneaux d'un évènement
+router.get("/evenements/:idEvenement/creneaux", creneauxCtrl.getByEvent);
+//Créer un créneau selon l'évènement
+router.post("/evenements/:idEvenement/create", admin, creneauxCtrl.createCreneaux);
+//Supprimer un créneau
+router.delete("/evenements/:idEvenement/creneaux/:idCreneau", admin, creneauxCtrl.deleteCreneaux);
+//Met à jour un créneau
+router.put("/evenements/:idEvenement/creneaux/:idCreneau", admin, creneauxCtrl.updateCreneaux);
+
+//Réservation de créneaux, met à jour idGroupe
+router.put("/evenements/:idEvenement/creneaux/:idCreneau/reserver", creneauxCtrl.reserverCreneaux);
+//Annuler réservation, enlève l'idGroupe
+router.put("/evenements/:idEvenement/creneaux/:idCreneau/annulerreservation", creneauxCtrl.annulerReservationCreneaux);
 
 module.exports = router;
