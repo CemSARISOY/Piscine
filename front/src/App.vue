6607e8346df37991a30916e38564a9c3d0c00899
Cem SARISOY <cem.sarisoy@etu.umontpellier.fr>
2020-12-20 11:03:46 +0100

    perf: front token verification once

--- front/src/App.vue
+++ front/src/App.vue
@@ -6,10 +6,20 @@
 </template>
 <script>
 import menuComponent from "@/components/menu";
+import store from "./store"
+import axios from "axios"
 export default {
     name: "App",
     components: {
         menuComponent,
+    },
+    beforeMount(){
+        axios.get("http://localhost:3000/api/etudiants/token", {withCredentials:true})
+        .then( result => {
+            if(result.data.success){
+                store.commit("CONNECT", result.data.success)
+            }
+        })
     }
 }
 </script>
