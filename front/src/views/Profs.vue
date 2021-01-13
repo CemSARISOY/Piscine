bf615d6183f2af12efe7d468e9790f88be3f327e
Cem SARISOY <cem.sarisoy@etu.umontpellier.fr>
2021-01-13 21:43:49 +0100

    feat: liste prof fini

--- front/src/views/Profs.vue
+++ front/src/views/Profs.vue
@@ -2,6 +2,23 @@
     <div id="promos">
         <b-container>
             <b-card>
+                <b-form inline @submit.prevent="onSubmit" class="float-right">
+                    <label class="sr-only" for="nomP">Name</label>
+                    <b-form-input
+                    id="nomP"
+                    class="mb-2 mr-sm-2 mb-sm-0"
+                    v-model="nvNom"
+                    placeholder="Entrer le nom"
+                    ></b-form-input>
+                    <label class="sr-only" for="prenomP">Name</label>
+                    <b-form-input
+                    id="prenomP"
+                    class="mb-2 mr-sm-2 mb-sm-0"
+                    v-model="nvPrenom"
+                    placeholder="Entrer le prenom"
+                    ></b-form-input>
+                    <b-button type="submit" variant="primary">Ajouter</b-button>
+                </b-form>
                 <b-pagination
                 v-model="currentPage"
                 :total-rows="rows()"
@@ -27,6 +44,8 @@ import axios from 'axios'
 export default {
     data(){
         return{
+            nvNom: '',
+            nvPrenom: '',
             perPage: 6,
             currentPage: 1,
             sortBy: '',
@@ -64,6 +83,15 @@ export default {
         },
         rows(){
             return this.profs.length
+        },
+        onSubmit(){
+            axios.post("http://localhost:3000/api/professeurs", {nomProf: this.nvNom, prenomProf: this.nvPrenom}, {withCredentials:true})
+            .then( () => {
+                this.$router.go();
+            })
+            .catch( (error) => {
+                this.makeToast(error.response.data.message)
+            });
         }
     }
 }
