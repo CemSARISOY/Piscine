a5372c1ffaac594293d1c7a21f86d9e66e54071e
afafbentouil <afaf.bentouil@gmail.com>
2021-01-14 00:11:42 +0100

    maj heures

--- front/src/components/planningForm.vue
+++ front/src/components/planningForm.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="center-div">
+      <!--b-button @click="$bvModal.show('modal-scoped')">Open Modal</b-button-->
         <b-modal
           id="modal-prevent-closing"
           ref="modal"
@@ -73,6 +74,45 @@
             <!--/b-form-group-->
           </form>
         </b-modal>
+        <b-modal ref="modal-scoped">
+          <template #modal-header="{ close }">
+            <h5>Voulez-vous vraiment supprimer ce creneau ?</h5>
+            <!-- Emulate built in modal header close button action -->
+            <b-button size="sm" variant="outline-danger" aria-label="Close" @click="close()">
+              <span aria-hidden="true">&times;</span>
+            </b-button>
+          </template>
+
+          <template #default="{ hide }">
+            <p>Creneau du {{dateCreneau}} de 9h00 à 10h00</p>
+            <b-button @click="hide()" variant="danger">
+              Supprimer
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+              </svg>
+            </b-button>
+          </template>
+
+          <template #modal-footer="{ ok, cancel }">
+            <!-- Emulate built in modal footer ok and cancel button actions -->
+            <b-button size="sm" variant="primary" @click="ok()">
+              Modifier
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
+                <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"/>
+                <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z"/>
+              </svg>
+            </b-button>
+            <b-button size="sm" variant="dark" @click="cancel()">
+              Annuler
+
+            </b-button>
+            <!-- Button with custom close trigger value -->
+            <!--b-button size="sm" variant="outline-secondary" @click="hide('forget')">
+              Forget it
+            </b-button-->
+          </template>
+        </b-modal>
         <FullCalendar :options="calendarOptions" />
     </div>
 </template>
@@ -98,6 +138,7 @@
     },
     data() {
       return {
+        dateCreneau:"",
         form:{
                 juryOptions: [],
                 jurySelected:[]
@@ -129,6 +170,7 @@
           initialView: 'timeGridWeek',
           allDaySlot: false,
           selectable: true,
+          editable: false,
           dateClick: this.testClick,//dateClick,
           eventClick: this.handleEventClick,
           events: [
@@ -141,10 +183,12 @@
             //{ title: 'event 2', date: '2021-01-07'}//display: 'background' }
           ],
           weekends: false, // initial value as you want it or not 
-          slotMinTime: "07:00",
+          slotMinTime: "00:00",
           slotMaxTime:"20:30",
           selectable: true,//initial value for selecting or not 
-          locale: initialLocaleCode
+          locale: initialLocaleCode,
+          date: "",
+          heures:""
         }
       }
     },
@@ -178,35 +222,44 @@
         // Hide the modal manually
         this.$nextTick(() => {
           this.$bvModal.hide('modal-prevent-closing')
-          console.log(this.$refs['my-modal'].value) //affiche la valeur de salle
-          console.log(this.value)
-          
+          this.salle = this.$refs['my-modal'].value //affiche la valeur de salle
+          console.log(this.calendarOptions.events)
+          //console.log(this.value)
+          if (this.value){
+              this.calendarOptions.heures = this.value
+              //console.log(this.calendarOptions.heures)
+          }
+          axios.post("http://localhost:3000/api/creneaux",{date:this.calendarOptions.date, heureDebut:this.calendarOptions.heures, salle:this.salle, idEvent:this.$route.params.id}, {withCredentials: true
+                }).then(response =>{
+                    // console.log('bien vu')
+                    // let creneau = {
+                    // title: response.salle, 
+                    // date:arg.date.toLocaleDateString(),
+                    // eventColor: 'green'}
+
+                    // console.log("Creneau crée !")
+                    // this.calendarOptions.events.push(creneau)
+                    // console.log(this)
+                    this.$router.go()//pour actualiser
+                }).catch(function(){
+                    console.log("Erreur de création")
+                })
+                console.log(this.form.juryOptions.text) 
         })
       },
       testClick: function(arg) {
         if(this.$store.getters.userInfo.isAdmin){
           this.$bvModal.show('modal-prevent-closing') 
-          //console.log(arg.date.toLocaleDateString())
-          axios.post("http://localhost:3000/api/evenements/:id/creneaux/",{date:arg.date.toLocaleDateString(), heureDebut:arg.date.toLocaleTimeString(), salle:this.value, idEvent:this.$route.params.id}, {withCredentials: true
-                }).then(response =>{
-                    console.log('bien vu')
-                    let creneau = {
-                    title: response.value, 
-                    date:arg.date.toLocaleDateString(),
-                    eventColor: 'green'}
+          this.calendarOptions.date = arg.date.toLocaleDateString()
+          this.calendarOptions.heures = arg.date.toLocaleTimeString()
 
-                    console.log("Creneau crée !")
-                    this.calendarOptions.events.push(creneau)
-                    console.log(this)
-                    this.$router.go()
-                }).catch(function(){
-                    console.log("Erreur de création")
-                })
-        }   
+          
+          }
+      },   
 
         //j = this.submittedJury.push(this.jury)
         //console.log(this.$refs['my-modal'].show())
-      },
+
       setNow() {
         const now = new Date()
         // Grab the HH:mm:ss part of the time string
@@ -215,59 +268,25 @@
       clearTime() {
         this.value = ''
       },
+      handleEventClick: function(arg){ 
+          // if(this.$store.getters.userInfo.isAdmin){
+          //   alert("test") 
+          // }
+        this.dateCreneau = arg.event.start
+        this.$refs['modal-scoped'].show()
+        let eventObj =arg.event
+        //alert('Event Name = ' + eventObj.id )
 
-      // dateClick: function(arg) {
-      //    if(this.$store.getters.userInfo.isAdmin){
-      //       //console.log("connected")
-      //       //let entree = prompt("")
-      //       this.$bvModal.show('modal-prevent-closing') 
-      //       if (this.handleSubmit()){
-      //           console.log("bien vu")
-      //           //recuperer jury dans la bdd 
-      //           axios.post("http://localhost:3000/api/evenements/:id/creneaux/",{date:arg.date.toLocaleDateString(), heureDebut:arg.date.toLocaleTimeString(), salle:this.value, idEvent:this.$route.params.id}, {withCredentials: true
-      //           }).then(response =>{
-      //               let creneau = {
-      //               title: response.value, 
-      //               date:arg.date.toLocaleDateString(),
-      //               eventColor: 'green'}
-
-      //               console.log("Creneau crée !")
-      //               this.calendarOptions.events.push(creneau)
-      //               console.log(this)
-      //               this.$router.go()
-      //           }).catch(function(){
-      //               console.log("Erreur de création")
-      //           })
-      //       }
-      //   }
-      // },
-      handleEventClick: function(){ 
-          if(store.getters.userInfo.isAdmin){
-            alert("test") 
-          }
+        console.log(arg.event.remove(arg.event.id))
+        // axios.delete("http://localhost:3000/api/creneaux/",{date:this.calendarOptions.date, heureDebut:this.calendarOptions.heures, salle:this.salle, idEvent:this.$route.params.id}, {withCredentials: true
+        //         }).then(response =>{
+                    
+        //             this.$router.go()//pour actualiser
+        //         }).catch(function(){
+        //             console.log("Erreur de création")
+        //         })
       }
     }, 
-    //get creneaux
-    async mounted(){
-        try{
-            const result = await axios.get("http://localhost:3000/api/evenements/"+this.$route.params.id+"/creneaux", {withCredentials: true
-              });
-            for(let i=0; i<result.data.length;i++){
-                let creneau = {
-                  title: result.data[i].idCreneau, 
-                  date: result.data[i].date,
-                  start: result.data[i].heureDebut,
-                  eventColor: 'green'}
-                console.log(creneau)
-                this.calendarOptions.events.push(creneau)
-
-            }
-            console.log(result)
-            console.log(this.calendarOptions.events)
-        }catch(err){
-            console.log(err)
-        }
-    },
     //get prof 
     async mounted(){
         try{
@@ -276,6 +295,21 @@
                 let option = {value: result.data[i].idProf, text: result.data[i].nomProf + " " + result.data[i].prenomProf}
                 this.form.juryOptions.push(option)
             }
+            console.log("r")
+            result = await axios.get("http://localhost:3000/api/evenements/"+this.$route.params.id+"/creneaux", {withCredentials: true
+              });
+            for(let i=0; i<result.data.length;i++){
+                let creneau = {
+                  id: result.data[i].idCreneau,
+                  title: result.data[i].salle, 
+                  //date: result.data[i].date,
+                  start: result.data[i].date.split("T")[0]+"T"+result.data[i].heureDebut+"Z",//result.data[i].date +'T'+ result.data[i].heureDebut+'Z','2021-01-14T12:30:00Z'
+                  eventColor: 'green'}
+                console.log(result.data[0].date.split("T")[0]+"T"+result.data[0].heureDebut+"Z")
+                this.calendarOptions.events.push(creneau)
+
+            }
+            //console.log(result.data[0].date.split("T")[0]+"T"+result.data[0].heureDebut+"Z"+ " "+result.data[0].salle)
         }catch(err){
             console.log(err)
         }
@@ -290,7 +324,7 @@
 <style >
   .center-div
 {
-  width: 70%;
+  width: 85%;
   display: block;
   margin-left: auto;
   margin-right: auto 
