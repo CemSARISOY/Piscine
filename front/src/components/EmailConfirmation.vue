1a7af09b395fc0ef9cd1be0f2ffd556275571aac
MarouanLaroui <60624791+MarouanLaroui@users.noreply.github.com>
2021-01-14 17:37:52 +0100

    Add files via upload

---
+++ front/src/components/EmailConfirmation.vue
@@ -0,0 +1,33 @@
+<template>
+<p> Confirmation de votre mail en cours... </p>
+</template>
+
+<script>
+import axios from 'axios'
+
+
+
+export default {
+  data() {
+
+  },
+  methods: {
+    onSubmit() {
+      axios.post("http://localhost:3000/api/etudiants/confirmation/:token")
+        .then( result => {
+          console.log(result.data.success)
+          this.$router.push("/")
+      }).catch( error => {
+        console.log("erreur : ", error);
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+    .form{
+        width:50%;
+        margin:auto
+    }
+</style>
