6aa6720b97da1bcf98557a1ee60ab11dcc114d69
Cem SARISOY <cem.sarisoy@etu.umontpellier.fr>
2021-01-15 22:57:43 +0100

    deploy

--- api/index.js
+++ api/index.js
@@ -49,7 +49,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/../front/dist/index.html'))
 })
 // Server
-const port = 3000
+const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
